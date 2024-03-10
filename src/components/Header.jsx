65b8b912f088840395a35fb57e76a9d0242ccb54
sarsi <sarasaravevic@gmail.com>
2024-03-10 14:23:10 +0100

    adding header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import reactImg from "../assets/react-core-concepts.png";
+const reactDescriptions = ["Fundemental", "Core", "Crucial"];
+function genRandmInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const description = reactDescriptions[genRandmInt(2)];
+  return (
+    <header>
+      <img src={reactImg} />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
